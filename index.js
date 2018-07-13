0e19a8788faa2c11c824db33706827ae47ba3375
Joshua Marshall Moore <thwee.abacadabra.alchemist@gmail.com>
2018-07-13 12:04:37 -0700

    initial synching doesn't work yet

--- index.js
+++ index.js
@@ -15,11 +15,13 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')));
 
+/*
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
+*/
 
 http.listen(PORT, function(){
   console.log(`listening on ${PORT}`);
@@ -29,8 +31,10 @@ http.listen(PORT, function(){
 io.on('connection', function(socket){
   console.log('a user connected');
 
-  socket.on('board-id', function(info){
-    socket.join(info.board_id);
+  socket.on('table', function(table){
+    console.log('table', table);
+    socket.join(room);
+    $(document).attr('title', `${table} - Chess - Social Cartography`);
   });
   
   socket.on('board-piece-position', info => {
