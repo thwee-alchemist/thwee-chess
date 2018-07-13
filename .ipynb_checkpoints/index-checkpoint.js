810dc5534c3765f458d25027846552432ee5bcce
Joshua Marshall Moore <thwee.abacadabra.alchemist@gmail.com>
2018-07-12 20:39:22 -0700

    initial import

---
+++ .ipynb_checkpoints/index-checkpoint.js
@@ -0,0 +1,45 @@
+/*
+  index.js
+  Joshua Marshall Moore
+  July 12th, 2018
+
+  North Las Vegas, Nevada
+
+  Goal of this server is to relay messages between chess players. 
+*/
+
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(PORT, function(){
+  console.log(`listening on ${PORT}`);
+});
+
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  
+  
+
+  socket.on('board-id', function(info){
+    socket.join(info.board_id);
+  });
+  
+  socket.on('board-piece-position', info => {
+    socket.join(info.board);
+    sokcet.emit('piece-move', info.piece)
+  })
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
